087df2cc655b4418979ac6dcce3fc13423be973b
John Hill <john.m.hill91@gmail.com>
2019-03-24 19:44:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,4 +5,12 @@
   def array_with_two_elements
   array=["basketball","football"]
   end
- def first_element()
+ def first_element(array)
+   array(0)
+ end
+ def third_element(array)
+   array(2)
+ end
+ def last_element(array)
+   array(-1)
+ end
